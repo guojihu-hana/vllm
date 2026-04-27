0dde99ba034ad8dad719ae6f7a7aa13c4f963a23
guojihu-hana <guojihu@pjlab.org.cn>
2026-04-27 19:19:31 +0800

    Init

---
+++ rlaunch_worker.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+GPU_NUM=${1:-2}
+CPU_NUM=${2:-$((GPU_NUM * 8))}
+MEMORY=${3:-$((GPU_NUM * 64000))}
+echo "GPU_NUM: ${GPU_NUM}, CPU_NUM: ${CPU_NUM}, MEMORY: ${MEMORY}"
+
+rlaunch --gpu=${GPU_NUM} \
+    --cpu=${CPU_NUM} \
+    --memory=${MEMORY} \
+    --private-machine=yes \
+    --charged-group=stu \
+    --positive-tags node/gpu-lg-cmc-h-h200-1086.host.h.pjlab.org.cn \
+    --mount=gpfs://gpfs1/ailab-sys/guojihu:/mnt/shared-storage-user/ailab-sys/guojihu \
+    --mount=gpfs://gpfs2/gpfs2-shared-public:/mnt/shared-storage-gpfs2/gpfs2-shared-public \
+    --image=registry.h.pjlab.org.cn/ailab-sys-sys_gpu/megatron:25.12-py3-nvshmem \
+    --workdir=/mnt/shared-storage-user/ailab-sys/guojihu/vllm \
+    --entrypoint /bin/bash
+
